4f6859623d71ab93dc5632324f228bfacd9337a0
nART <152645786+nARTnn@users.noreply.github.com>
2024-07-01 11:02:04 +0200

    Add files via upload
    Final calculator

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -2,24 +2,29 @@
 
 calculate = ""
 
-def add_to_calculate(symbol):
+def add_to_calculate(symbol, text_res):
     global calculate
     calculate += str(symbol)
     text_res.delete(1.0, "end")
     text_res.insert(1.0, calculate)
 
-def calculation():
+def calculation(text_res):
     global calculate
     try:
         calculate = str(eval(calculate))
         text_res.delete(1.0, "end")
         text_res.insert(1.0, calculate)
-    except:
-        clear()
+    except Exception:
+        clearAll(text_res)
         text_res.insert(1.0, "Error")
 
-def clear():
+def clearAll(text_res):
     global calculate
     calculate = ""
     text_res.delete(1.0, "end")
-
+    
+def clear(text_res, d : int):
+    global calculate
+    calculate = calculate[0:-1]
+    text_res.delete(1.0, "end")
+    text_res.insert("end", calculate)
